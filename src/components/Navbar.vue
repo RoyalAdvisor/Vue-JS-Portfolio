1219753fa4682b519f697d3d7de9720e7a304329
Nadeem Johnson <91249112+RoyalAdvisor@users.noreply.github.com>
2022-01-29 19:41:43 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -206,10 +206,10 @@ nav a:hover {
   .menu-container {
     background-color: #000;
     width: 100%;
-    height: 80px;
+    height: 60px;
     padding: 20px;
     position: fixed;
-    top: 1%;
+    top: 0;
     display: flex;
     justify-content: flex-start;
     align-items: center;
