8be0d7a574ec84fc698ab1dd1c45e552e1d41c8d
Nadeem Johnson <91249112+RoyalAdvisor@users.noreply.github.com>
2022-01-29 19:59:03 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -313,7 +313,7 @@ export default {
 }
 @media only screen and (max-width: 576px) {
   .about-container {
-    margin-top: 22rem;
+    margin-top: 24rem;
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
